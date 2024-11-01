e6ec4edf060c6fb5a1363bb448c730b2c3eabb04
Peter Lilley <hi.petelilley@gmail.com>
2024-10-31 23:25:48 -0400

    Created package

---
+++ f1tenth_pure_pursuit/pure_pursuit.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from f1tenth_pure_pursuit.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'f1tenth_pure_pursuit'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Peter Lilley',
+    maintainer_email='peterlil@buffalo.edu',
+    description='Pure pursuit algorithm implementatino for F1Tenth racecar',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pure_pursuit = f1tenth_pure_pursuit.pure_pursuit:main',
+            'test_waypoints = f1tenth_pure_pursuit.test_waypoints:main'
+        ],
+    },
+)
